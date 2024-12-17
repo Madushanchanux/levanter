9760f7eec6e2d9894e97c2b477568bdca5a53e63
Chanuka-madushan <chanukamadushan889@gmail.com>
2024-12-17 20:55:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
 process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_17ca975514f1d242a1bb22fc58c665cf00').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
